ec1677036e23a1f7eaefd9042f24c6e5d5021a70
Frédérick Armando <frederickarmando@gmail.com>
2017-08-25 21:07:21 +0200

    devtools for VueJS

--- js/index.js
+++ js/index.js
@@ -1,5 +1,10 @@
+// Devtools for development builds, set false for production builds
+Vue.config.devtools = true
+
+// Import VueMaterial
 Vue.use(VueMaterial)
 
+// Create Vue
 var App = new Vue({
   el: '#main'
 })
